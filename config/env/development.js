4257d9309348139caf2a252bdf781071f560c9c5
skison <samuel.j.kison@wmich.edu>
2018-02-21 16:46:57 -0500

    updated facebook callback
    Now allows for AWS EC2 connection

--- config/env/development.js
+++ config/env/development.js
@@ -4,6 +4,6 @@ module.exports = {
 	facebook: {
 		clientID: '1650915691656530',
 		clientSecret: 'ded57c7e6431f4c4d1927c76eedd50b1',
-		callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+		callbackURL: 'http://ec2-18-219-123-26.us-east-2.compute.amazonaws.com:3000/oauth/facebook/callback'
 	}
 };
